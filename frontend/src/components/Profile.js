41153766353ba1736063ef31dd7e4004aac7c948
ArvinSamiei <samiei.arvin@gmail.com>
2020-02-03 23:09:41 +0330

    make profile look better

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -1,44 +1,37 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { fetchUser } from "../actions";
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
+import './Profile.css'
 
 export class Profile extends Component {
-
-    componentDidMount(){
-        this.props.fetchUser(localStorage.getItem('username'))
-    }
+	componentDidMount() {
+		this.props.fetchUser(localStorage.getItem("username"));
+	}
 
 	render() {
-        if (!this.props.user){
-            return null;
-        }
+		if (!this.props.user) {
+			return null;
+		}
 		return (
-			<div className="row">
-				<div className="col-xs-12 col-sm-6 col-md-6">
-					<div className="well well-sm">
-						<div className="row">
-							<div className="col-sm-6 col-md-4">
-								<img
-									src={`http://localhost:8000/users/images/${this.props.user.username}`}
-									alt=""
-									className="img-rounded img-responsive"
-								/>
-							</div>
-							<div className="col-sm-6 col-md-8">
-								<h4>{`${this.props.user.first_name} ${this.props.user.last_name}`}</h4>
-								<p>
-									<i className="glyphicon glyphicon-envelope"></i>{this.props.user.email}
-									<br />
-									
-								</p>
-							</div>
-						</div>
-					</div>
-                    <Link className='btn btn-primary' to='/createPost'>
-                        Create Post
-                    </Link>
-				</div>
+			<div className="card" style={{float: 'left'}}>
+				<img
+					style={{ width: "100%" }}
+					src={`http://localhost:8000/users/images/${this.props.user.username}`}
+					alt=""
+					className="img-rounded img-responsive"
+				/>
+				<h1>{`${this.props.user.first_name} ${this.props.user.last_name}`}</h1>
+				<p className="title">
+					<img src="https://img.icons8.com/cotton/64/000000/email-open.png" />
+					{this.props.user.email}
+					<br />
+				</p>
+				<p>
+					<Link to="/createPost">
+						Create Post
+					</Link>
+				</p>
 			</div>
 		);
 	}
@@ -50,4 +43,4 @@ const mapStateToProps = state => {
 	};
 };
 
-export default connect(mapStateToProps, {fetchUser})(Profile);
+export default connect(mapStateToProps, { fetchUser })(Profile);
